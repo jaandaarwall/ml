ede1202c6a121bf96c43ad2dfb6655c8d61510b6
jaandaarwall <23f1003066@ds.study.iitm.ac.in>
2025-11-15 10:58:52 +0530

    removed the some commented part from backend and also removed wrong to creating doctor and patient because some table filed get emty which cause error

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,3 @@
-# class Config:
-#     SECRET_KEY = 'your_secret_key_here'
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///HMS.db'
-
-#     SECURITY_PASSWORD_SALT = 'your_password_salt_here'
-#     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authorization'
-
-
 class Config:
     SECRET_KEY = 'your_secret_key_here_change_in_production'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///HMS.db'
